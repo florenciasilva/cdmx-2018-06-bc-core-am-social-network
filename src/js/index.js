9f8dc0edede7327e5c4097915b606f3bf2fd8c4a
Hensgrej <hensgrej@gmail.com>
2018-07-26 11:14:26 -0500

    Cambio de división de tareas en archivos, se agrega botón de sign out, se arregla autenticación de sign up

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+// Button Events
 document.getElementById('signIn').style.display = 'none';
 document.getElementById('hide-sign-up').addEventListener('click', (ev) => {
     event.preventDefault(ev);
@@ -5,23 +6,6 @@ document.getElementById('hide-sign-up').addEventListener('click', (ev) => {
     document.getElementById('sign-up').style.display = 'none';
     document.getElementById('signIn').style.display = 'block';
 });
-
-// HTML Container for Sign Up and Sign In
-let signInAndUpContainer = document.getElementById('sign-in-and-sign-up');
-// HTML Container for News Feed
-let newsFeedContainer = document.getElementById('news-feed');
-// HTML Elements for Sign Up
-let btnSignUp = document.getElementById('btnSignUp');
-let userNickname = document.getElementById('icon_prefix');
-let userEmail = document.getElementById('icon_email');
-let userPassword = document.getElementById('icon_vpn_key');
-// HTML Elements for Sign In/Out
-let btnSignIn = document.getElementById('btnSignIn');
-let userEmailSignIn = document.getElementById('icon_email_sign_in');
-let userPasswordSignIn = document.getElementById('icon_vpn_key_sign_in');
-let btnSignInGoogle = document.getElementById('btn_sign_in_google');
-let btnSignOut = document.getElementById('btn_sign_out');
-// Button Events
 
 // Register New User
 btnSignUp.addEventListener('click', (ev) => {
@@ -31,7 +15,6 @@ btnSignUp.addEventListener('click', (ev) => {
     // Creating user with Email and Password
     firebase.auth().createUserAndRetrieveDataWithEmailAndPassword(userEmailValue, userPasswordValue).then(function() {
         window.location.assign('../src/news-feed.html');
-
     }).catch(function(error) {
         var errorCode = error.code;
         var errorMessage = alert(error.message);
@@ -68,7 +51,7 @@ btnSignIn.addEventListener('click', (ev) => {
     let userPasswordValueSignIn = userPasswordSignIn.value;
     // Sign In Firebase Function
     firebase.auth().signInWithEmailAndPassword(userEmailValueSignIn, userPasswordValueSignIn).then(function(success) {
-        // Managin HTML view
+        // Managing HTML view
         signInAndUpContainer.style.display = 'none';
         newsFeedContainer.style.display = 'block';
         document.getElementById('userNickname').style.display = 'block';
@@ -79,45 +62,8 @@ btnSignIn.addEventListener('click', (ev) => {
     });
     firebase.auth().signInWithEmailAndPassword(userEmailValueSignIn, userPasswordValueSignIn);
 });
-
-// Log in with facebook, we get this code from the site of facebook for developers
-/* let provider = new firebase.auth.FacebookAuthProvider();
-firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-  let token = result.credential.accessToken;
-  // The signed-in user info.
-  let user = result.user;
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-  let errorCode = error.code;
-  let errorMessage = error.message;
-  // The email of the user's account used.
-  let email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  let credential = error.credential;
-  // ...
-  return provider;
-  console.log(provider);
-});
-
-// sign out
-firebase.auth().signOut().then(function() {
-  // Sign-out successful.
-}).catch(function(error) {
-  // An error happened.
-});
-
-// Google Sign In
- btnSignInGoogle.addEventListener('click', (ev) => {
-    event.preventDefault(ev);
-    // Sign In with Google (Code Snippet from Firebase Documentation)
-    // Initialize the FirebaseUI Widget using Firebase.
-    let ui = new firebaseui.auth.AuthUI(firebase.auth());
-    // The start method will wait until the DOM is loaded.
-    ui.start('#firebaseui-auth-container', uiConfig);
-
-    initApp = function() {
+/*
+const initApp = () => {
         firebase.auth().onAuthStateChanged(function(user) {
             if (user) {
                 // User is signed in.
@@ -155,28 +101,4 @@ firebase.auth().signOut().then(function() {
 
     window.addEventListener('load', function() {
         initApp();
-    });
-        let provider = new firebase.auth.GoogleAuthProvider();
-          firebase.auth().signInWithPopup(provider).then(function(result) {
-              console.log(provider);
-              // This gives you a Google Access Token. You can use it to access the Google API.
-              let token = result.credential.accessToken;
-              // The signed-in user info.
-              let user = result.user;
-          }).catch(function(error) {
-              // Handle Errors here.
-              let errorCode = error.code;
-              let errorMessage = error.message;
-              // The email of the user's account used.
-              let email = error.email;
-              // The firebase.auth.AuthCredential type that was used.
-              let credential = error.credential;
-          });
-
-          firebase.auth().signInWithPopup(provider);
-});
-
-// btnSignOut.addEventListener('click', () => {
-
-// });
-*/
+    });*/
---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+// HTML Elements
+
+// HTML Container for Sign Up and Sign In
+let signInAndUpContainer = document.getElementById('sign-in-and-sign-up');
+// HTML Container for News Feed
+let newsFeedContainer = document.getElementById('news-feed');
+// HTML Elements for Sign Up
+let btnSignUp = document.getElementById('btnSignUp');
+let userNickname = document.getElementById('icon_prefix');
+let userEmail = document.getElementById('icon_email');
+let userPassword = document.getElementById('icon_vpn_key');
+// HTML Elements for Sign In/Out
+let btnSignIn = document.getElementById('btnSignIn');
+let userEmailSignIn = document.getElementById('icon_email_sign_in');
+let userPasswordSignIn = document.getElementById('icon_vpn_key_sign_in');
+let btnSignInGoogle = document.getElementById('btn_sign_in_google');
+let btnSignOut = document.getElementById('btn_sign_out');
+
+// HTML Elements for Comment Section
+let btnSendComment = document.getElementById('btnSendComment');
+let commentArea = document.getElementById('commentArea');
+let printComment = document.getElementById('printMessage');
+
